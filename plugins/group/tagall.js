79046f5f1c351c17f2aebb4c25e839549eacd4ff
ALFIXD <99767269+Fiisya@users.noreply.github.com>
2025-04-05 18:43:31 +0700

    update

---
+++ plugins/group/tagall.js
@@ -0,0 +1,29 @@
+//Simple Base Botz
+// • Credits : wa.me/62895322391225 [ Asyl ]
+// • Feature : group/tagall
+
+
+let handler = async (m, {
+    conn,
+    text,
+    participants
+}) => {
+    let teks = `⋙ *PESAN DARI ADMIN GROUP* ⋘
+\n *${text ? text : 'Nothing'}*\n\n`
+    for (let mem of participants) {
+        teks += ` @${mem.id.split('@')[0]}\n`
+    }
+    teks += `___________________________________________`
+    conn.sendMessage(m.chat, {
+        text: teks,
+        mentions: participants.map(a => a.id)
+    }, )
+}
+handler.help = ['tagall <pesan>']
+handler.tags = ['group']
+handler.command = /^(tagall)$/i
+
+handler.group = true
+handler.admin = true
+
+module.exports = handler
